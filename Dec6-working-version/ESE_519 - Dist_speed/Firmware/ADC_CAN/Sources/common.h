75bd90b8cd05293c35294f749e48f9fda141292c
Anand Madhusoodanan <anand.madhusoodanan1988@gmail.com>
2012-12-06 22:26:58 -0500

    Latest Working Code included

---
+++ Dec6-working-version/ESE_519 - Dist_speed/Firmware/ADC_CAN/Sources/common.h
@@ -0,0 +1,16 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+typedef unsigned char UINT8;        /*unsigned 8 bit definition */
+typedef unsigned short UINT16;      /*unsigned 16 bit definition*/
+typedef unsigned long UINT32;       /*unsigned 32 bit definition*/
+typedef signed char INT8;           /*signed 8 bit definition */
+typedef short INT16;                /*signed 16 bit definition*/
+typedef long int INT32;             /*signed 32 bit definition*/
+
+#define TRUE    1
+#define FALSE   0
+
+#define F_BUS (2000000UL)
+
+#endif
